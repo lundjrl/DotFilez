cdace20749c0fc93cfdd1ce4393916231c83bb38
lundjrl <jrlbidamin@gmail.com>
2023-08-06 11:50:54 -0400

    Added pre-commit hooks

--- scripts/sync-vscode.sh
+++ scripts/sync-vscode.sh
@@ -1,14 +1,17 @@
 #!/bin/zsh
 
-if [[ ! -d "../Shared/vscode" ]] then
+if [[ ! -d "./Shared/vscode" ]]
+then
 	echo "File doesn't exist. Creating"
-	mkdir "../Shared/vscode"
-	echo "File created"
+	mkdir ./Shared
+	mkdir ./Shared/vscode
+  echo "File created"
 fi
 
 echo "Syncing files"
-cp  ~/Library/Application\ Support/Code/User/settings.json ../Shared/vscode/settings.json
+cp  ~/Library/Application\ Support/Code/User/settings.json ./Shared/vscode/settings.json
 
-cp ~/Library/Application\ Support/Code/User/keybindings.json ../Shared/vscode/keybindings.json
+cp ~/Library/Application\ Support/Code/User/keybindings.json ./Shared/vscode/keybindings.json
 
+git add -u ./Shared
 echo "Synced"
